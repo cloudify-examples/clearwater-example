225e43b076740c270a4807087de23ea284294101
Anna <anna.szpoton@gigaspaces.com>
2016-09-30 17:09:46 +0200

    Add stress test example blueprint

--- scripts/clearwater/stress-test/bulk-provisioning-numbers-homestead.sh
+++ scripts/clearwater/stress-test/bulk-provisioning-numbers-homestead.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-NAME="users"
 ctx logger info "Generate users CSV file"
-. /etc/clearwater/config; for DN in {2010000000..2010099999} ; do echo sip:$DN@$home_domain,$DN@$home_domain,$home_domain,7kkzTyGW ; done > ${NAME}.csv
+. /etc/clearwater/config; for DN in {2010000000..2010099999} ; do echo sip:$DN@$home_domain,$DN@$home_domain,$home_domain,7kkzTyGW ; done > ${name}.csv
 
 ctx logger info "Run bulk_create.py "
-/usr/share/clearwater/crest/src/metaswitch/crest/tools/bulk_create.py ${NAME}.csv
+/usr/share/clearwater/crest/src/metaswitch/crest/tools/bulk_create.py ${name}.csv
 
 python -m SimpleHttpServer 53229
 #${NAME}.create_xdm.cqlsh
 #${NAME}.create_xdm.sh
 
-ctx logger info "Run ${NAME}.create_homestead.sh "
-./${NAME}.create_homestead.sh
+ctx logger info "Run ${name}.create_homestead.sh "
+./${name}.create_homestead.sh
