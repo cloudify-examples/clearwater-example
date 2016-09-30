9bc583420c0f87c8ea362bfbe0cb8dbfebcf9d11
Anna <anna.szpoton@gigaspaces.com>
2016-09-30 15:17:25 +0200

    Add snmpd installation

--- scripts/clearwater/sprout/install-sprout.sh
+++ scripts/clearwater/sprout/install-sprout.sh
@@ -31,6 +31,7 @@ sudo apt-get update
 ctx logger info "Installing sprout packages and other clearwater packages"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install sprout --yes --force-yes -o DPkg::options::=--force-confnew
 sudo DEBIAN_FRONTEND=noninteractive  apt-get install clearwater-management --yes --force-yes
+sudo DEBIAN_FRONTEND=noninteractive apt-get install clearwater-snmpd --yes --force-yes
 ctx logger info "The installation packages is done correctly"
 
 ctx logger info "Use the DNS server"
