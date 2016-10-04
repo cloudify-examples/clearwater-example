df2b88b869cfd1709d84ef377e9bf6da21f01677
Anna <anna.szpoton@gigaspaces.com>
2016-10-04 11:35:37 +0200

    Add stress-test blueprint

--- scripts/clearwater/stress-test/bulk-provisioning-numbers-homestead.sh
+++ scripts/clearwater/stress-test/bulk-provisioning-numbers-homestead.sh
@@ -6,9 +6,9 @@ ctx logger info "Generate users CSV file"
 ctx logger info "Run bulk_create.py "
 /usr/share/clearwater/crest/src/metaswitch/crest/tools/bulk_create.py ${name}.csv
 
-python -m SimpleHttpServer 53229
+python -m SimpleHttpServer 53229 &
 #${NAME}.create_xdm.cqlsh
 #${NAME}.create_xdm.sh
 
 ctx logger info "Run ${name}.create_homestead.sh "
-./${name}.create_homestead.sh
+bash ${name}.create_homestead.sh
--- scripts/clearwater/stress-test/install-stress-test.sh
+++ scripts/clearwater/stress-test/install-stress-test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-ctx logger debug "${COMMAND}"
-
 ctx logger info "Configure the APT software source"
 if [ ! -f /etc/apt/sources.list.d/clearwater.list ]
   then
@@ -12,8 +10,15 @@ sudo apt-get update
 
 ctx logger info ""
 
-sudo /etc/clearwater/local_config
+sudo tee -a /etc/clearwater/local_config << EOF
+local_ip=${homestead_ip}
+EOF
 
+sudo tee -a /etc/clearwater/shared_config << EOF
+home_domain=${home_domain}
+bono_servers=[${bono_servers}]
+count=${number_of_subscribers}
+EOF
 
 ctx logger info "Installing stress test package"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install clearwater-sip-stress --yes --force-yes -o DPkg::options::=--force-confnew
