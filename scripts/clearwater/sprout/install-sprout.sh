2a703cfb2bbe4a2d9eaed34083ca32f32483e968
Anna <anna.szpoton@gigaspaces.com>
2016-10-18 18:22:14 +0200

    Update to use Clearwater release-108

--- scripts/clearwater/sprout/install-sprout.sh
+++ scripts/clearwater/sprout/install-sprout.sh
@@ -29,7 +29,7 @@ fi
 sudo apt-get update
 
 ctx logger info "Installing sprout packages and other clearwater packages"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install sprout --yes --force-yes -o DPkg::options::=--force-confnew
+sudo DEBIAN_FRONTEND=noninteractive apt-get install sprout-node --yes --force-yes -o DPkg::options::=--force-confnew
 sudo DEBIAN_FRONTEND=noninteractive  apt-get install clearwater-management --yes --force-yes
 sudo DEBIAN_FRONTEND=noninteractive apt-get install clearwater-snmpd --yes --force-yes
 ctx logger info "The installation packages is done correctly"
